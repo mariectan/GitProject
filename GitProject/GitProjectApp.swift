7d535f4ba9a56a9eb86740319c641ae695b7cec6
Apprenant63 <tanmariec@gmail.com>
2024-03-06 13:25:27 +0100

    First Commit

---
+++ GitProject/GitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectApp.swift
+//  GitProject
+//
+//  Created by Apprenant63 on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
